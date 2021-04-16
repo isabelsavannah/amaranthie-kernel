6653f328033247398746d5040f7361c660539e57
isabel <isabel@izabyl.com>
2021-04-16 22:24:48 +0000

    more half finished peering

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,15 @@
+import * as random from "./random.js"
+
 export default {
+  id: random.id(),
+
   gossip: {
     listenPort: 1234,
-    intervalSeconds: 10
+    gossipIntervalMillis: 30000,
+    aliveGraceMillis: 65000,
+    forgetGraceMillis: 600000,
+    connectionRetryBaseMillis: 5000,
+    connectionRetryBackoffFactor: 2,
+    nonceWindow: 3,
   },
-  id: "9999",
 }
--- src/gossip_peers.ts
+++ src/gossip_peers.ts
@@ -14,37 +14,108 @@ interface PeerAddress {
   port: string
 }
 
-interface PeerState {
-  address: PeerAddress
-  lastNonceSent: number
-  lastNonceRecvd: number
-  lastKnownLive: number
+interface Peer {
+  readonly address: PeerAddress
+  readonly lastKnownLive: number
+  readonly knownLive: boolean
+  readonly useSocket: (socket: Socket) => void
 }
+
+function initPeer(peer: PeerAddress, destroy: () => void): Peer {
+  let lastNonceSent = []
+  let lastNonceRecvd = 0
+  let socket = null
+  let openSocketAttempts = 0
+
+  let publicState = {
+    address: peer,
+    knownLive: false,
+    lastKnownLive: Date.now(),
+    useSocket: () => void,
+  }
+
+  async function runPeer() {
+    while(true) {
+      try {
+        let socket = await getSocket()
+      } catch (err) {
+        destroy()
+        break
+      }
+
+
+      
+    getSocket()
+      .then(/*send heartbeat messages until error/*, reject)
+      .catch(
+
+
+  function getSocket(attemptNumber: number = 1): Promise<Socket> {
+    let attempt = Promise((resolve, reject) => {
+      let candidateSocket = new net.Socket()
+      candidateSocket.on('connect', () => resolve(candidateSocket))
+      candidateSocket.on('error', reject)
+    })
+
+    if(attemptNumber >= config.gossip.connectionMaxRetries) {
+      return attempt
+    } else {
+      let delayFactor = config.gossip.connectionRetryBackoffFactor ** (attemptNumber - 1)
+      let delay = config.gossip.connectionRetryBaseMillis * delayFactor
+      return attempt
+        .catch(() => timersPromises.setTimeout(delay))
+        .then(() => getSocket(attemptNumber+1))
+    }
+  }
+
+  function tick(socket: Socket) {
+    // send heartbeat
+    // then weait
+    // then repeat
+    // if we fail, open a new socket
+    // 
+
+
+  function run() {
+    getSocket()
+      .then(
+
+  async function tend() {
+    while(true) {
+      let socket = await getSocket()
+
+      
+
+
+
+}
+    
+
 
 export function GossipPeers(): Net{
   log("initializing net")
 
-  let peers = []
-  let pendingPeers = []
-  let liveConnections = {}
-
-  function getConnection(
+  let peers: {[key: string]: PeerState} = {}
+  let pendingPeers: [PeerAddress] = {}
 
   function evaluateNewPeer() {
-    if(!pendingPeers.length) {
+   if(!pendingPeers.length) {
       return
 
     let candidatePeer = pendingPeers.pop()
     if(peers.some(peer => peer.id === candidatePeer.id))
-      evalu
+      evaluateNewPeer()
+
+    peers[peer.id] = initPeer(peer)
+  }
 
   function tick() {
-    evaluateNewPeer
-    
+    //...
+  }
 
   return {
     start: function() {
-      return
+      tick()
     },
 
     introduce: function (host: string, port: number) {
